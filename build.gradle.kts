a10143bd4f1f5a29a99a824b11bebee5c14b46ec
nnrogers515 <nnrogers515@gmail.com>
2020-04-24 18:44:07 -0500

    Hope

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.3.71"
+//    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "org.example"
@@ -11,6 +13,15 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    val ktorVersion = "1.3.0"
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("org.slf4j:slf4j-simple:1.7.26")
 }
 
 tasks {
@@ -19,5 +30,25 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+val mainClass = "KotlinDataScience.MainKt"
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = mainClass
     }
 }
