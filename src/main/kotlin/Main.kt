3386b39184b6e394e2db5791307bc65d3fc6ad7b
nnrogers515 <nnrogers515@gmail.com>
2020-04-24 17:48:17 -0500

    still busted working on this

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+fun Application.data() {
+    routing {
+        get("/") {
+            call.respondText("OK")
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::data).start(wait = true)
+}
