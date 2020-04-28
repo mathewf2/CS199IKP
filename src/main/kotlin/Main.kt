e46658a60a1117d1428c1a5e065ebe999a38c3c6
nnrogers515 <nnrogers515@gmail.com>
2020-04-27 19:46:16 -0500

    feat: Created a home informational page as well as a survey page

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,12 +28,80 @@ fun Application.data() {
                     link(rel = "stylesheet", href = "/static/main.css")
                 }
 
+                body(classes = "mainBody") {
+                    div {
+                        div("row") {
+                            div("buttonGrid homeC") {
+                                a("/", classes = "topButtons") {
+                                    +"Home"
+                                }
+
+                                a("Survey", classes = "topButtons") {
+                                    +"Survey"
+                                }
+
+                                a("Data", classes = "topButtons") {
+                                    +"Data"
+                                }
+                            }
+                        }
+
+                        div(classes = "iframeHolder home") {
+                            img {
+                                src = "/static/cs199projectbanner.png"
+                            }
+
+                            p(classes = "description") {
+                                +"The purpose of this project is to visualize a dataset of responses regarding the effects of COVID-19. The data is collected from students, friends, relatives, and anyone who is willing to respond, and it is represented using a variety of Kotlin data science libraries such as Let's Plot, Kmath and Knumpy. This visual representation is hosted via KTOR, and the page was built using HTML DSL for KTOR."
+                            }
+
+                            p(classes = "description") {
+                                +"If you are interested in seeing and/or editing the code for the project, here is a link to our "
+
+                                a(href =  "https://github.com/mathewf2/CS199IKP", target = "_blank", classes = "githubLink") {
+                                    +"Public Github"
+                                }
+                            }
+
+                            p(classes = "description") {
+                                +"Thank you for taking the time to explore information and provide your feedback on the impact of Covid-19 on your life, and We hope for you to stay safe and maintain compassion during these frustrating times."
+                            }
+                        }
+                    }
+                    script(type = ScriptType.textJavaScript) {
+                        unsafe {
+                            raw(
+                                """
+                                    function my() { return 1; }
+                                    
+                                    """
+                            )
+                        }
+                    }
+                    div (classes = "footer") {
+                        h4 (classes = "credit") {
+                            +"A UIUC CS199IKP Project by Mathew Farley and Noah Rogers"
+                        }
+                    }
+                }
+            }
+        }
+        get("/Survey") {
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "/static/main.css")
+                }
+
                 body {
                     div {
                         div("row") {
                             div("buttonGrid homeC") {
                                 a("/", classes = "topButtons") {
                                     +"Home"
+                                }
+
+                                a("Survey", classes = "topButtons") {
+                                    +"Survey"
                                 }
 
                                 a("Data", classes = "topButtons") {
@@ -44,7 +112,7 @@ fun Application.data() {
 
                         div(classes = "iframeHolder") {
                             h1("title") {
-                                +"Kotlin Data Science Project: CS199IKP"
+                                +"Please Take Our Survey! Share Your Opinions!"
                             }
                             iframe() {
                                 src =
@@ -82,6 +150,10 @@ fun Application.data() {
                             div("buttonGrid homeC") {
                                 a("/", classes = "topButtons") {
                                     +"Home"
+                                }
+
+                                a("Survey", classes = "topButtons") {
+                                    +"Survey"
                                 }
 
                                 a("Data", classes = "topButtons") {
